603233aa845c2fa8b6cfcf911f6b38eb8ab4e076
Ahmed <ahmed0033ahmed.mo@gmail.com>
2024-11-05 17:28:03 +0200

    update readme.md and add start page point php page

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Student with PHP
+
+<ul>
+  <li>
+    <ul>
+      <li>Server Data</li>
+      <ul>
+        <li>Type: Exampp</li>
+        <li>Programming Language: PHP</li>
+        <li>Username: root</li>
+        <li>Password: empty</li>
+        <li>Database: student</li>
+        <li>Table: student</li>
+        <ul>
+          <li>Columns:</li>
+          <ul>
+            <li>ID (int, Auto Increment, Primary Key)</li>
+            <li>Name (varchar)</li>
+            <li>Address (varchar)</li>
+          </ul>
+        </ul>
+      </ul>
+    </ul>
+  </li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Student </title>
+    </head>
+
+    <body>
+        <dev>
+            <div id="mother">
+                <form method="POST">
+                    <aside>
+                        <div id="div">
+                            <img src="" alt="Web Logo">
+                            <h3>manager panel</h3>
+                            <label for="">Student ID :</label><br>
+                            <input type="text" name="ID" id="ID"><br>
+                            <label for="">Student Name :</label><br>
+                            <input type="text" name="Name" id="Name"><br>
+                            <label for="">Student Address :</label><br>
+                            <input type="text" name="Address" id="Address"><br>
+                        </div>
+                    </aside>
+                    <main>
+                    </main>
+                </form>
+            </div>
+        </dev>
+    </body>
+
+</html>
